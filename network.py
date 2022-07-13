c421f43e2d9057c77771b732a70ef5238fddc569
Håkon Hove <hbhove@gmail.com>
2022-07-13 09:29:26 +0200

    adding function feedforward (half finished)

--- network.py
+++ network.py
@@ -24,3 +24,11 @@ class Network(object):
         self.biases = [np.random.randn(y, 1) for y in sizes[1:]]
         self.weights = [np.random.randn(y, x)
                         for x, y in zip(sizes[:-1], sizes[1:])]
+
+
+    def feedforward(self, a):
+         for b, w in zip(self.biases, self.weights):
+            a = sigmoid(np.dot(w, a)+b)
+        return
+
+        
